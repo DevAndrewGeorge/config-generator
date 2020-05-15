bebc89403c23d20dcef34db66b5fa0061ec5b0cd
Andrew George <devAndrewGeorge@pm.me>
2020-05-15 00:54:34 -0700

    Add test for parsing plugins config

--- internal/pkg/parser/plugins_test.go
+++ internal/pkg/parser/plugins_test.go
@@ -8,108 +8,123 @@ import (
 func TestParsePlugin (t *testing.T) {
   var empty_config map[string]interface{}
   t.Run("plugin: console", func(t *testing.T) {
-    g, e := parsePlugin("console", empty_config)
+    p, e := parsePlugin("console", empty_config)
     if e != nil {
       t.Fail()
     }
 
-    if _, ok := g.(plugins.ConsolePlugin); !ok {
+    if _, ok := p.(*plugins.ConsolePlugin); !ok {
       t.Fail()
     }
   })
 
   t.Run("plugin: environment", func(t *testing.T) {
-    g, e := parsePlugin("environment", empty_config)
+    p, e := parsePlugin("environment", empty_config)
 
     if e != nil {
       t.Fail()
     }
 
-    if _, ok := g.(plugins.EnvironmentPlugin); !ok {
+    if _, ok := p.(*plugins.EnvironmentPlugin); !ok {
       t.Fail()
     }
   })
 
   t.Run("plugin: file", func(t *testing.T) {
-    g, e := parsePlugin("file", empty_config)
+    p, e := parsePlugin("file", empty_config)
 
     if e != nil {
       t.Fail()
     }
 
-    if _, ok := g.(plugins.FilePlugin); !ok {
+    if _, ok := p.(*plugins.FilePlugin); !ok {
       t.Fail()
     }
   })
 
   t.Run("plugin: http", func(t *testing.T) {
-    g, e := parsePlugin("http", empty_config)
+    p, e := parsePlugin("http", empty_config)
 
     if e != nil {
       t.Fail()
     }
 
-    if _, ok := g.(plugins.HttpPlugin); !ok {
+    if _, ok := p.(*plugins.HttpPlugin); !ok {
       t.Fail()
     }
   })
 
   t.Run("plugin: kubernetes", func(t *testing.T) {
-    g, e := parsePlugin("kubernetes", empty_config)
+    p, e := parsePlugin("kubernetes", empty_config)
 
     if e != nil {
       t.Fail()
     }
 
-    if _, ok := g.(plugins.KubernetesPlugin); !ok {
+    if _, ok := p.(*plugins.KubernetesPlugin); !ok {
       t.Fail()
     }
   })
 
   t.Run("plugin: multi", func(t *testing.T) {
-    g, e := parsePlugin("multi", empty_config)
+    p, e := parsePlugin("multi", empty_config)
 
     if e != nil {
       t.Fail()
     }
 
-    if _, ok := g.(plugins.MultiPlugin); !ok {
+    if _, ok := p.(*plugins.MultiPlugin); !ok {
       t.Fail()
     }
   })
 
   t.Run("plugin: script", func(t *testing.T) {
-    g, e := parsePlugin("script", empty_config)
+    p, e := parsePlugin("script", empty_config)
 
     if e != nil {
       t.Fail()
     }
 
-    if _, ok := g.(plugins.ScriptPlugin); !ok {
+    if _, ok := p.(*plugins.ScriptPlugin); !ok {
       t.Fail()
     }
   })
 
   t.Run("plugin: static", func(t *testing.T) {
-    g, e := parsePlugin("static", empty_config)
+    p, e := parsePlugin("static", empty_config)
 
     if e != nil {
       t.Fail()
     }
 
-    if _, ok := g.(plugins.StaticPlugin); !ok {
+    if _, ok := p.(*plugins.StaticPlugin); !ok {
       t.Fail()
     }
   })
 
   t.Run("plugin: vault", func(t *testing.T) {
-    g, e := parsePlugin("vault", empty_config)
+    p, e := parsePlugin("vault", empty_config)
 
     if e != nil {
       t.Fail()
     }
 
-    if _, ok := g.(plugins.VaultPlugin); !ok {
+    if _, ok := p.(*plugins.VaultPlugin); !ok {
+      t.Fail()
+    }
+  })
+
+  t.Run("plugin: configures plugin", func(t *testing.T) {
+    config := map[string]interface{}{"file": "hello"}
+    expected := plugins.NewEnvironmentPlugin()
+    expected.Configure(config)
+    p, e := parsePlugin("environment", config)
+
+    if e != nil {
+      t.Fail()
+    }
+
+    if p == nil || !p.Equal(expected) {
       t.Fail()
     }
   })
