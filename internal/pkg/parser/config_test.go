9099858c2ff9f6cf5db3047dff4dd71d0f3f24d7
Andrew George <devAndrewGeorge@pm.me>
2020-05-18 22:23:27 -0700

    Add getPlugin tests

--- internal/pkg/parser/config_test.go
+++ internal/pkg/parser/config_test.go
@@ -1,1 +1,36 @@
 package parser
+
+import(
+  "testing"
+  "github.com/devandrewgeorge/config-generator/internal/pkg/plugins"
+)
+
+func TestNamedPluginConfigGetPlugin(t *testing.T) {
+  t.Run("no plugin configuration", func(t *testing.T){
+    input := &namedPluginConfig{}
+    if _, err := input.getPlugin("test"); err == nil {
+      t.Fail()
+    }
+  })
+
+  t.Run("one plugin configuration", func(t *testing.T){
+    input := &namedPluginConfig{
+      Environment: &environmentPluginConfig{},
+    }
+    expected := &plugins.EnvironmentPlugin{}
+    expected.Configure("test", nil)
+
+    result, err := input.getPlugin("test")
+
+    if err != nil || !result.Equal(expected) { t.Fail() }
+  })
+
+  t.Run("multiple plguin configurations", func(t *testing.T){
+    input := &namedPluginConfig{
+      Environment: &environmentPluginConfig{},
+      Console: &consolePluginConfig{},
+    }
+
+    if _, err := input.getPlugin("test"); err == nil { t.Fail() }
+  })
+}
