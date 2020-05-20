d7b5343cf351bf7b4e494ed8aa7be1e4ad69f932
Andrew George <devAndrewGeorge@pm.me>
2020-05-19 22:06:07 -0700

    Define Template struct

--- internal/pkg/templates/NestedTemplate.go
+++
@@ -1,7 +0,0 @@
-package templates
-
-type NestedTemplate struct {}
-
-func (n NestedTemplate) Equal(o Template) bool {
-  return o != nil && Template(n) == o
-}
--- internal/pkg/templates/templates.go
+++ internal/pkg/templates/templates.go
@@ -1,5 +1,33 @@
 package templates
 
-type Template interface {
-  Equal(Template) bool
+func New(name string, data interface{}) (*Template, error) {
+    return nil, nil
+}
+
+type Template struct {
+    name string
+    text *string
+    templates map[string]*template
+}
+
+func (t *Template) Render(variables map[string]string) (string, error) {
+    return "", nil
+}
+
+func (t *Template) RenderYaml(variables map[string]string) (string, error) {
+    return "", nil
+}
+
+func (t *Template) RenderJson(variables map[string]string) (string, error) {
+    return "", nil
+}
+
+func (t *Template) Equal(o *Template) bool {
+    if t.name != o.name { return false }
+    if t.text != o.text { return false }
+    if length(t.templates) != length(o.templates) { return false }
+    for k,v := range t.templates {
+        if v != o.templates[k] { return false }
+    }
+    return true
 }
